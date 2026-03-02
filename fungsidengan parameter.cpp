c62099574717d3904b97e5c65d0b7ac4215e6666
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-03-02 15:50:31 +0700

    mplementasi fungsi dan prosedur

--- fungsidengan parameter.cpp
+++ fungsidengan parameter.cpp
@@ -3,3 +3,10 @@
 using namespace std;
 //deklarasi variable global
 int panjang, lebar;
+//implementasi fungsi dan prosedur
+void input(){
+    cout <<"Masukan Panjang :";
+    cin >> panjang;
+    cout <<"Masukan Lebar :";
+    cin >> lebar;
+}
